136e82b66d1eb6e4a207ae6a5c52cd8c7e4678ba
ItaySN <itayfic@gmail.com>
2020-10-28 22:30:45 +0200

    Updating

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,111 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Charts from './components/Charts';
+import Select from 'react-select';
+
 
 function App() {
-  return (
-    <Charts/>
-  );
+  const [data, setData] = useState<unknown>();
+  const [countries, setCountries] = useState<{label:string,value:number}[]>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [firstCountryName, setFirstCountryName] = useState<string>('ISRAEL')
+  const [secondCountryName, setSecondCountryName] = useState<string>('US')
+  const [firstCountryIndex,setFirstSelect] = useState<number>(149)
+  const [secondCountryIndex,setSecondSelect] = useState<number>(244)
+  
+
+  //Get data from the CSV for two countries
+  const reRender = async () =>{
+    try{
+      fetch('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+        .then(res => res.text())
+        .then(resData => {
+          console.log(resData)
+          
+          const dates: string[] = resData.split('\n').slice(0).map(line => (line.split(',')))[0].slice(4);
+
+          //Get the data of the two countries by their index
+          const firstCountry: string[] = resData.split('\n').slice(0).map(line => (line.split(',')))[firstCountryIndex];
+          const secondCountry: string[] = resData.split('\n').slice(0).map(line => (line.split(',')))[secondCountryIndex];
+          
+          const firstCountryName1 = firstCountry[1].toUpperCase();
+          
+          const secondCountryName1 = secondCountry[1].toUpperCase();
+
+          const formatData: any = [];
+         
+          for (let i = 4; i < dates.length; i++) {
+            let newRow: any = {};
+            newRow["date"] = dates[i];
+            newRow[firstCountryName1] = parseInt(firstCountry[i]);
+            newRow[secondCountryName1] = parseInt(secondCountry[i]);            
+            formatData.push(newRow);
+          }
+
+          
+          const countriesList: string[][] = resData.split('\n').slice(1).map(line => (line.split(',').slice(0, 2)));
+          const countriesArr: { label: string, value: number }[] = [];
+
+          for (let i = 0; i < countriesList.length; i++) {
+            countriesArr.push({ label: countriesList[i].reverse().join(' : '), value: i + 1 })
+          }
+  
+          setData(formatData);
+          setFirstCountryName(firstCountryName1);
+          setSecondCountryName(secondCountryName1)
+          setCountries(countriesArr)
+          setIsLoading(false)
+        })
+    }
+    catch(err){
+      console.error(err.message)
+    }
+  }
+
+  useEffect(() =>{
+    reRender();
+  },[])
+
+  //When changing the first selcet value
+  const firstInput = (input:any) =>{
+    setFirstSelect(input.value)
+  }
+  //When changing the second selcet value
+  const secondInput = (input: any) => {
+    setSecondSelect(input.value)
+  }
+
+  useEffect(()=>{
+      reRender()
+   },[firstCountryIndex, secondCountryIndex])
+  
+
+  return(
+    <div className="App">
+      <h1>Covid-19 Confirmed Cases</h1>
+      {isLoading ? 
+        <div> loading...</div>
+      :
+      <>
+        {
+          countries &&
+          <>
+            <Charts data={data} label1={firstCountryName} lebel2={secondCountryName} />
+            <div>
+              <Select options={countries} onChange={firstInput} placeholder="Israel" />
+              <Select options={countries} onChange={secondInput} placeholder="USA" />
+            </div>
+          </>
+        }
+      </>
+    }
+    </div>
+  )
+  
 }
+
+
+
 
 export default App;
--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,24 +1,75 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useState,useCallback} from 'react';
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+import Select from 'react-select';
 
 
-function Charts() {
+
+
+
+function Charts({data,label1,lebel2}) {
     
-    const [data,setData] = useState<unknown>();
+    // const [data,setData] = useState<unknown>();
+    // const [countries,setCountries] = useState<any>();
+    // const [isLoading,setLoading] = useState<boolean>(true);
 
-    useEffect(()=>{
-        fetch('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-        .then(res=>res.text())
-        .then(res=>{
-            setData(res)
-        })
-        .catch(err =>{
-            console.log(err)
-        })
-    },[])
+    // const orderByCountry = useCallback((csv:string)=>{
+    //         var lines = csv.split("\n");
+    //         var counrties:any = [];
+
+    //         var result:any = [];
+
+    //         var headers = lines[0].split(",");
+            
+    //         for (var i = 1; i < lines.length; i++) {
+
+    //             var obj:any = {};
+    //             var currentline = lines[i].split(",");
+
+    //             for (var j = 0; j < headers.length; j++) {
+    //                 obj[headers[j]] = currentline[j];
+    //             }
+    //             counrties.push(obj['Country/Region'])
+    //             result.push(obj);
+    //         }
+    //         setCountries(counrties)
+    //         return result;
+    // },[])
+
+    // useEffect(()=>{
+    //     fetch('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+    //     .then(res=>res.text())
+    //     .then(res=>{
+
+    //         const lines:string[] = orderByCountry(res)
+    //         // console.log(lines)
+    //         setData(lines)
+    //     })
+    //     .catch(err =>{
+    //         console.log(err)
+    //     })
+    // },[])
+
+    // const dataCharting = (countries:Object[]) =>{
+            
+    // }
+
 
     return (
         <div>
-            
+            <div>
+                <LineChart width={800} height={500} data={data}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey={label1} stroke="#8884d8" activeDot={{r:8}} />
+                    <Line type="monotone" dataKey={lebel2} stroke="#82ca9d" activeDot={{ r: 8 }} />
+                </LineChart>
+            </div>
         </div>
     )
 }
